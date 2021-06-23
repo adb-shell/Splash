70de8d81ec7437157c47e7bd7e212c42c66c009a
Karthik Rk <karthik.rk18@gmail.com>
2021-06-23 18:01:48 +0530

    updating the appstore release variant

--- app/src/appstore/java/com.karthik.splash.root/NetworkModule.kt
+++ app/src/appstore/java/com.karthik.splash.root/NetworkModule.kt
@@ -1,26 +1,20 @@
 package com.karthik.splash.root
 
-import android.content.Context
+import com.karthik.network.utility.AuthorizationKeyInterceptor
+import com.karthik.network.utility.UserOfflineInterceptor
 import com.karthik.splash.BuildConfig
 import com.karthik.splash.misc.InternetHandler
-import com.karthik.splash.restserviceutility.interceptors.AuthorizationKeyInterceptor
-import com.karthik.splash.restserviceutility.interceptors.UserOfflineInterceptor
 import com.karthik.splash.storage.MemoryCache
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
 class NetworkModule {
-
-    private val TIME_OUT = 30L
-
+    
     @Named("BASE_URL")
     @Provides
     fun providesBaseURL() = BuildConfig.SPLASH_BASE_URL
